cc192f6a9795cd560986e80f3bf4ddc3f4af33f5
bedvoa <ujmn0418@gmail.com>
2024-09-02 23:09:52 +0900

    layer 분리

--- netflix/src/app.service.ts
+++ netflix/src/app.service.ts
@@ -1,8 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+export interface Movie {
+  id: number;
+  title: string;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private movies: Movie[] = [
+    { id: 1, title: '해리포터' },
+    { id: 2, title: '반지의 제왕' },
+  ];
+  private idCounter = 3;
+
+  getManyMovies(title?: string) {
+    if (!title) {
+      return this.movies;
+    }
+
+    return this.movies.filter((movie) => movie.title.startsWith(title));
+  }
+
+  getMovieById(id: string) {
+    const movie = this.movies.find((movie) => movie.id === +id);
+
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
+    }
+
+    return movie;
+  }
+
+  createMovie(title: string) {
+    const newMovie = { id: this.idCounter, title };
+
+    this.movies.push(newMovie);
+    this.idCounter += 1;
+
+    return newMovie;
+  }
+
+  updateMovie(id: string, title: string) {
+    const movie = this.movies.find((movie) => movie.id === +id);
+
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
+    }
+
+    Object.assign(movie, { title });
+
+    return movie;
+  }
+
+  deleteMovie(id: string) {
+    const movieIndex = this.movies.findIndex((movie) => movie.id === +id);
+
+    if (movieIndex === -1) {
+      throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
+    }
+
+    this.movies.splice(movieIndex, 1);
+
+    return id;
   }
 }
