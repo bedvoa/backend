c444f18e14aba7f55c60859c273a50ba90eb5933
bedvoa <ujmn0418@gmail.com>
2024-09-05 00:07:10 +0900

    custom transformer

--- netflix/src/movie/entity/movie.entity.ts
+++ netflix/src/movie/entity/movie.entity.ts
@@ -1,4 +1,4 @@
-// import { Exclude, Expose } from 'class-transformer';
+// import { Exclude, Expose, Transform } from 'class-transformer';
 
 // @Exclude() // 해당 클래스의 모든 속성을 숨기는 옵션
 export class Movie {
@@ -7,5 +7,6 @@ export class Movie {
 
   // @Expose() // 해당 클래스의 모든 속성을 숨기는 옵션이 설정되어 있을 때 해당 속성을 노출하는 옵션
   // @Exclude() // 해당 속성을 숨기는 옵션
+  // @Transform(({ value }) => value.toString().toUpperCase()) // 해당 속성의 값을 대문자로 변환하는 옵션
   genre: string;
 }
