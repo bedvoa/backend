3e642a525430d4510adb905434d7415cee486e68
bedvoa <ujmn0418@gmail.com>
2024-09-10 01:00:10 +0900

    feat: repository CRUD

--- netflix/src/movie/movie.controller.ts
+++ netflix/src/movie/movie.controller.ts
@@ -28,7 +28,7 @@ export class MovieController {
   // 특정 ID 값을 가진 영화를 반환하는 API
   @Get(':id')
   getMovie(@Param('id') id: string) {
-    return this.movieService.getMovieById(id);
+    return this.movieService.getMovieById(+id);
   }
 
   // 새로운 영화를 생성하는 API
@@ -40,12 +40,12 @@ export class MovieController {
   // 특정 ID 값을 가진 영화를 수정하는 API
   @Patch(':id')
   patchMovie(@Body() body: UpdateMovideDto, @Param('id') id: string) {
-    return this.movieService.updateMovie(id, body);
+    return this.movieService.updateMovie(+id, body);
   }
 
   // 특정 ID 값을 가진 영화를 삭제하는 API
   @Delete(':id')
   deleteMovie(@Param('id') id: string) {
-    return this.movieService.deleteMovie(id);
+    return this.movieService.deleteMovie(+id);
   }
 }
--- netflix/src/movie/movie.service.ts
+++ netflix/src/movie/movie.service.ts
@@ -2,85 +2,58 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovideDto } from './dto/update-movie.dto';
 import { Movie } from './entity/movie.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class MovieService {
-  private movies: Movie[] = [];
-  private idCounter = 3;
-
-  constructor() {
-    const movie1 = new Movie();
-    movie1.title = '해리포터';
-    movie1.id = 1;
-    movie1.genre = '판타지';
-
-    const movie2 = new Movie();
-    movie2.title = '반지의 제왕';
-    movie2.id = 2;
-    movie2.genre = '판타지';
-
-    this.movies.push(movie1, movie2);
-  }
+  constructor(
+    @InjectRepository(Movie)
+    private readonly movieRepository: Repository<Movie>,
+  ) {}
 
   // 모든 영화 목록을 반환하는 메서드
-  getManyMovies(title?: string) {
-    if (!title) {
-      return this.movies;
-    }
-
-    return this.movies.filter((movie) => movie.title.startsWith(title));
+  async getManyMovies(title?: string) {
+    return await this.movieRepository.find();
   }
 
   // 특정 ID 값을 가진 영화를 반환하는 메서드
-  getMovieById(id: string) {
-    const movie = this.movies.find((movie) => movie.id === +id);
+  async getMovieById(id: number) {
+    return await this.movieRepository.findOne({ where: { id } });
+  }
+
+  // 새로운 영화를 생성하는 메서드
+  async createMovie(createMovieDto: CreateMovieDto) {
+    const movie = await this.movieRepository.save({
+      ...createMovieDto,
+    });
+
+    return movie;
+  }
+
+  // 특정 ID 값을 가진 영화를 수정하는 메서드
+  async updateMovie(id: number, updateMovieDto: UpdateMovideDto) {
+    const movie = await this.movieRepository.findOne({ where: { id } });
 
     if (!movie) {
       throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
     }
 
-    return movie;
-  }
+    await this.movieRepository.update({ id }, { ...updateMovieDto });
 
-  // 새로운 영화를 생성하는 메서드
-  createMovie(createMovieDto: CreateMovieDto) {
-    const newMovie = {
-      id: this.idCounter,
-      ...createMovieDto,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      version: 0,
-    };
-
-    this.movies.push(newMovie);
-    this.idCounter += 1;
+    const newMovie = await this.movieRepository.findOne({ where: { id } });
 
     return newMovie;
   }
 
-  // 특정 ID 값을 가진 영화를 수정하는 메서드
-  updateMovie(id: string, updateMovieDto: UpdateMovideDto) {
-    const movie = this.movies.find((movie) => movie.id === +id);
+  // 특정 ID 값을 가진 영화를 삭제하는 메서드
+  async deleteMovie(id: number) {
+    const movie = await this.movieRepository.findOne({ where: { id } });
 
     if (!movie) {
       throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
     }
 
-    Object.assign(movie, updateMovieDto);
-
-    return movie;
-  }
-
-  // 특정 ID 값을 가진 영화를 삭제하는 메서드
-  deleteMovie(id: string) {
-    const movieIndex = this.movies.findIndex((movie) => movie.id === +id);
-
-    if (movieIndex === -1) {
-      throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
-    }
-
-    this.movies.splice(movieIndex, 1);
-
-    return id;
+    return await this.movieRepository.delete({ id });
   }
 }
